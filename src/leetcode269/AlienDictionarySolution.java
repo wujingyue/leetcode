109fdc6f6fa69e3f497277faba2bed20e773a833
Jingyue Wu <wujingyue@gmail.com>
2018-04-08 12:09:44 -0700

    Rename "marks" to "colors".

--- src/leetcode269/AlienDictionarySolution.java
+++ src/leetcode269/AlienDictionarySolution.java
@@ -17,12 +17,12 @@ class AlienDictionarySolution {
 			}
 		}
 
-		Color[] marks = new Color[26];
-		Arrays.fill(marks, Color.WHITE);
+		Color[] colors = new Color[26];
+		Arrays.fill(colors, Color.WHITE);
 		StringBuilder postOrder = new StringBuilder();
 		for (int i = 0; i < 26; ++i) {
 			if (used[i]) {
-				if (depthFirstSearch(i, partialOrder, marks, postOrder)) {
+				if (depthFirstSearch(i, partialOrder, colors, postOrder)) {
 					return "";
 				}
 			}
@@ -31,24 +31,24 @@ class AlienDictionarySolution {
 		return postOrder.reverse().toString();
 	}
 
-	boolean depthFirstSearch(int i, boolean[][] partialOrder, Color[] marks, StringBuilder postOrder) {
-		if (marks[i] == Color.GRAY) {
+	boolean depthFirstSearch(int i, boolean[][] partialOrder, Color[] colors, StringBuilder postOrder) {
+		if (colors[i] == Color.GRAY) {
 			return true;
 		}
-		if (marks[i] == Color.BLACK) {
+		if (colors[i] == Color.BLACK) {
 			return false;
 		}
 
-		assert marks[i] == Color.WHITE;
-		marks[i] = Color.GRAY;
+		assert colors[i] == Color.WHITE;
+		colors[i] = Color.GRAY;
 		for (int j = 0; j < 26; ++j) {
 			if (partialOrder[i][j]) {
-				if (depthFirstSearch(j, partialOrder, marks, postOrder)) {
+				if (depthFirstSearch(j, partialOrder, colors, postOrder)) {
 					return true;
 				}
 			}
 		}
-		marks[i] = Color.BLACK;
+		colors[i] = Color.BLACK;
 		postOrder.append((char) ('a' + i));
 		return false;
 	}
