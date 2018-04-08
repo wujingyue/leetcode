7f1913c285fb2450e6ff38701c0449b884500b25
Jingyue Wu <wujingyue@gmail.com>
2018-04-08 12:05:19 -0700

    Rename "precedes" to "order".

--- src/leetcode269/AlienDictionarySolution.java
+++ src/leetcode269/AlienDictionarySolution.java
@@ -8,7 +8,7 @@ class AlienDictionarySolution {
 	}
 
 	public String alienOrder(String[] words) {
-		boolean[][] precedes = collectPrecedence(words);
+		boolean[][] order = collectOrders(words);
 
 		boolean[] used = new boolean[26];
 		for (int i = 0; i < words.length; ++i) {
@@ -22,7 +22,7 @@ class AlienDictionarySolution {
 		StringBuilder postOrder = new StringBuilder();
 		for (int i = 0; i < 26; ++i) {
 			if (used[i]) {
-				if (depthFirstSearch(i, precedes, marks, postOrder)) {
+				if (depthFirstSearch(i, order, marks, postOrder)) {
 					return "";
 				}
 			}
@@ -31,7 +31,7 @@ class AlienDictionarySolution {
 		return postOrder.reverse().toString();
 	}
 
-	boolean depthFirstSearch(int i, boolean[][] precedes, Color[] marks, StringBuilder postOrder) {
+	boolean depthFirstSearch(int i, boolean[][] order, Color[] marks, StringBuilder postOrder) {
 		if (marks[i] == Color.GRAY) {
 			return true;
 		}
@@ -42,8 +42,8 @@ class AlienDictionarySolution {
 		assert marks[i] == Color.WHITE;
 		marks[i] = Color.GRAY;
 		for (int j = 0; j < 26; ++j) {
-			if (precedes[i][j]) {
-				if (depthFirstSearch(j, precedes, marks, postOrder)) {
+			if (order[i][j]) {
+				if (depthFirstSearch(j, order, marks, postOrder)) {
 					return true;
 				}
 			}
@@ -53,8 +53,8 @@ class AlienDictionarySolution {
 		return false;
 	}
 
-	boolean[][] collectPrecedence(String[] words) {
-		boolean[][] precedes = new boolean[26][26];
+	boolean[][] collectOrders(String[] words) {
+		boolean[][] order = new boolean[26][26];
 		for (int i = 0; i + 1 < words.length; ++i) {
 			String a = words[i];
 			String b = words[i + 1];
@@ -68,8 +68,8 @@ class AlienDictionarySolution {
 
 			assert j < a.length();
 			assert j < b.length();
-			precedes[a.charAt(j) - 'a'][b.charAt(j) - 'a'] = true;
+			order[a.charAt(j) - 'a'][b.charAt(j) - 'a'] = true;
 		}
-		return precedes;
+		return order;
 	}
 }
