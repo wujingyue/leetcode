75384e9ef4b3158c400bc7452f3fa1ffc6c1afb4
Jingyue Wu <wujingyue@gmail.com>
2019-09-14 11:32:22 -0700

    LeetCode 1187 -- Make Array Strictly Increasing.

---
+++ 1187.cc
@@ -0,0 +1,73 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int makeArrayIncreasing(vector<int> a, vector<int> b) {
+    vector<int> nums(a);
+    nums.insert(nums.end(), b.begin(), b.end());
+    sort(nums.begin(), nums.end());
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());
+
+    int n = a.size();
+    for (int i = 0; i < n; i++) {
+      a[i] = lower_bound(nums.begin(), nums.end(), a[i]) - nums.begin();
+    }
+    for (int i = 0; i < b.size(); i++) {
+      b[i] = lower_bound(nums.begin(), nums.end(), b[i]) - nums.begin();
+    }
+    sort(b.begin(), b.end());
+
+    vector<int> next_in_b(nums.size());
+    for (int j = 0; j < nums.size(); j++) {
+      auto next = upper_bound(b.begin(), b.end(), j);
+      next_in_b[j] = (next == b.end() ? -1 : *next);
+    }
+
+    // m[i][j] represents the fewest replacements to make a[0..i] increasing and
+    // a[i] equal to j.
+    vector<vector<int>> m(n, vector<int>(nums.size(), INT_MAX));
+    m[0][a[0]] = 0;
+    if (!b.empty()) {
+      UpdateIfGreaterThan(&m[0][b[0]], 1);
+    }
+    for (int i = 0; i + 1 < n; i++) {
+      for (int j = 0; j < nums.size(); j++) {
+        if (m[i][j] == INT_MAX) {
+          continue;
+        }
+        if (j < a[i + 1]) {
+          UpdateIfGreaterThan(&m[i + 1][a[i + 1]], m[i][j]);
+        }
+        int next_j = next_in_b[j];
+        if (next_j >= 0) {
+          UpdateIfGreaterThan(&m[i + 1][next_j], m[i][j] + 1);
+        }
+      }
+    }
+    int answer = INT_MAX;
+    for (int j = 0; j < nums.size(); j++) {
+      UpdateIfGreaterThan(&answer, m[n - 1][j]);
+    }
+    return (answer == INT_MAX ? -1 : answer);
+  }
+
+ private:
+  void UpdateIfGreaterThan(int* a, int b) {
+    if (*a > b) {
+      *a = b;
+    }
+  }
+};
+
+int main() {
+  Solution s;
+  cout << s.makeArrayIncreasing({1, 5, 3, 6, 7}, {1, 3, 2, 4}) << endl;
+  cout << s.makeArrayIncreasing({1, 5, 3, 6, 7}, {4, 3, 1}) << endl;
+  cout << s.makeArrayIncreasing({1, 5, 3, 6, 7}, {1, 6, 3, 3}) << endl;
+  cout << s.makeArrayIncreasing({1, 5, 3, 6, 7}, {}) << endl;
+}
