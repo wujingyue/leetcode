b88113e103d7228c109b04f445bdcc7755e7a951
Jingyue Wu <wujingyue@gmail.com>
2019-10-13 07:57:39 -0700

    Memoized BFS.
    Even slower unfortunately.

--- 980.cc
+++ 980.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 
 #include "gtest/gtest.h"
@@ -8,83 +9,82 @@ using namespace std;
 constexpr int kDx[] = {-1, 0, 1, 0};
 constexpr int kDy[] = {0, 1, 0, -1};
 
-class Solver {
- public:
-  Solver(const vector<vector<int>>& grid) : grid_(grid) {
-    r_ = grid.size();
-    c_ = grid[0].size();
-    m_.resize(r_);
-    for (int x = 0; x < r_; x++) {
-      m_[x].resize(c_);
-      for (int y = 0; y < c_; y++) {
-        m_[x][y].resize(1 << r_ * c_, -1);
-      }
-    }
-  }
+struct Node {
+  bool operator<(const Node& other) const { return s > other.s; }
 
-  int Solve() {
-    int start_x = -1;
-    int start_y = -1;
-    int end_x = -1;
-    int end_y = -1;
-    int end_s = 0;
-    for (int x = 0; x < r_; x++) {
-      for (int y = 0; y < c_; y++) {
-        if (grid_[x][y] == 1) {
-          start_x = x;
-          start_y = y;
-        }
-        if (grid_[x][y] == 2) {
-          end_x = x;
-          end_y = y;
-        }
-        if (grid_[x][y] != -1) {
-          end_s |= (1 << (x * c_ + y));
-        }
-      }
-    }
-
-    m_[start_x][start_y][1 << (start_x * c_ + start_y)] = 1;
-    return DFS(end_x, end_y, end_s);
-  }
-
- private:
-  int DFS(int x, int y, int s) {
-    if (m_[x][y][s] >= 0) {
-      return m_[x][y][s];
-    }
-
-    int mask = (1 << (x * c_ + y));
-    if ((s & mask) == 0) {
-      return 0;
-    }
-
-    m_[x][y][s] = 0;
-    for (int dir = 0; dir < 4; dir++) {
-      int x2 = x + kDx[dir];
-      int y2 = y + kDy[dir];
-      if (x2 < 0 || x2 >= r_ || y2 < 0 || y2 >= c_) {
-        continue;
-      }
-      if (grid_[x2][y2] == -1) {
-        continue;
-      }
-      m_[x][y][s] += DFS(x2, y2, s - mask);
-    }
-    return m_[x][y][s];
-  }
-
-  vector<vector<int>> grid_;
-  vector<vector<vector<int>>> m_;
-  int r_;
-  int c_;
+  int x;
+  int y;
+  int s;
 };
 
 class Solution {
  public:
   int uniquePathsIII(const vector<vector<int>>& grid) {
-    Solver solver(grid);
-    return solver.Solve();
+    int r = grid.size();
+    int c = grid[0].size();
+    int start_x = -1;
+    int start_y = -1;
+    int end_x = -1;
+    int end_y = -1;
+    int end_s = 0;
+    for (int x = 0; x < r; x++) {
+      for (int y = 0; y < c; y++) {
+        if (grid[x][y] == 1) {
+          start_x = x;
+          start_y = y;
+        }
+        if (grid[x][y] == 2) {
+          end_x = x;
+          end_y = y;
+        }
+        if (grid[x][y] != -1) {
+          end_s |= (1 << (x * c + y));
+        }
+      }
+    }
+
+    vector<vector<vector<int>>> m(
+        r, vector<vector<int>>(c, vector<int>(1 << r * c)));
+    priority_queue<Node> q;
+
+    int start_s = (1 << (start_x * c + start_y));
+    m[start_x][start_y][start_s] = 1;
+    q.push(Node{start_x, start_y, start_s});
+    while (!q.empty()) {
+      const Node& top = q.top();
+      const int x = top.x;
+      const int y = top.y;
+      const int s = top.s;
+      const int value = m[x][y][s];
+      q.pop();
+
+      if (s >= end_s) {
+        break;
+      }
+
+      for (int dir = 0; dir < 4; dir++) {
+        int x2 = x + kDx[dir];
+        int y2 = y + kDy[dir];
+        if (x2 < 0 || x2 >= r || y2 < 0 || y2 >= c) {
+          continue;
+        }
+        if (grid[x2][y2] == -1) {
+          continue;
+        }
+        int mask2 = (1 << (x2 * c + y2));
+        if (s & mask2) {
+          continue;
+        }
+        int s2 = s + mask2;
+        int& value2 = m[x2][y2][s2];
+        if (value2 == 0) {
+          q.push(Node{x2, y2, s2});
+        }
+        value2 += value;
+      }
+    }
+
+    return m[end_x][end_y][end_s];
   }
 };
 
