19b8bd2b8c7b75dc34f8300b04997edff12962d0
Jingyue Wu <wujingyue@gmail.com>
2020-06-09 22:35:59 -0700

    Easier to understand.

--- 337.cc
+++ 337.cc
@@ -12,12 +12,15 @@ struct TreeNode {
 
 class Solution {
  public:
-  int rob(const TreeNode* root) { return MaxRob(root).second; }
+  int rob(const TreeNode* root) {
+    pair<int, int> res = MaxRob(root);
+    return max(res.first, res.second);
+  }
 
  private:
   // Returns a pair of integers. The first integer means the max money robbed if
-  // p's parent is robbed. The second integer means the max money robbed if p's
-  // parent is not robbed.
+  // p is robbed. The second integer means the max money robbed if p is not
+  // robbed.
   pair<int, int> MaxRob(const TreeNode* p) {
     if (p == nullptr) {
       return {0, 0};
@@ -25,9 +28,9 @@ class Solution {
 
     pair<int, int> left_res = MaxRob(p->left);
     pair<int, int> right_res = MaxRob(p->right);
-    const int parent_is_robbed = left_res.second + right_res.second;
-    const int parent_is_not_robbed =
-        max(parent_is_robbed, p->val + left_res.first + right_res.first);
-    return {parent_is_robbed, parent_is_not_robbed};
+    const int robbed = p->val + left_res.second + right_res.second;
+    const int not_robbed = max(left_res.first, left_res.second) +
+                           max(right_res.first, right_res.second);
+    return {robbed, not_robbed};
   }
 };
