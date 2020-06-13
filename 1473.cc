b65d178d48ea079e43751e2fcff07f251b9d0e52
Jingyue Wu <wujingyue@gmail.com>
2020-06-12 19:47:56 -0700

    Space = O(num_neighborhoods * num_colors).

--- 1473.cc
+++ 1473.cc
@@ -20,65 +20,63 @@ class Solution {
       }
     }
 
-    vector<vector<vector<int>>> m(
-        num_houses, vector<vector<int>>(num_neighborhoods + 1,
-                                        vector<int>(num_colors + 1, INT_MAX)));
-    vector<vector<vector<int>>> prefix_min(
-        num_houses, vector<vector<int>>(num_neighborhoods + 1,
-                                        vector<int>(num_colors + 1, INT_MAX)));
-    vector<vector<vector<int>>> suffix_min(
-        num_houses, vector<vector<int>>(num_neighborhoods + 1,
-                                        vector<int>(num_colors + 1, INT_MAX)));
+    vector<vector<int>> m(num_neighborhoods + 1,
+                          vector<int>(num_colors + 1, INT_MAX));
+    vector<vector<int>> prefix_min(num_neighborhoods + 1,
+                                   vector<int>(num_colors + 1, INT_MAX));
+    vector<vector<int>> suffix_min(num_neighborhoods + 1,
+                                   vector<int>(num_colors + 1, INT_MAX));
     if (houses[0] > 0) {
-      m[0][1][houses[0]] = 0;
+      m[1][houses[0]] = 0;
     } else {
       for (int k = 1; k <= num_colors; k++) {
-        m[0][1][k] = cost[0][k - 1];
+        m[1][k] = cost[0][k - 1];
       }
     }
-    ComputePrefixSuffixMin(0, 1, m, prefix_min, suffix_min);
+    ComputePrefixSuffixMin(1, m, prefix_min, suffix_min);
 
     for (int i = 1; i < n; i++) {
       for (int j = 1; j <= num_neighborhoods; j++) {
         for (int k = 1; k <= num_colors; k++) {
           if (cost[i][k - 1] == INT_MAX) {
+            m[j][k] = INT_MAX;
             continue;
           }
 
-          int local_min = m[i - 1][j][k];
-          local_min = min(local_min, prefix_min[i - 1][j - 1][k - 1]);
+          int local_min = min(m[j][k], prefix_min[j - 1][k - 1]);
           if (k + 1 <= num_colors) {
-            local_min = min(local_min, suffix_min[i - 1][j - 1][k + 1]);
+            local_min = min(local_min, suffix_min[j - 1][k + 1]);
           }
           if (local_min < INT_MAX) {
             local_min += cost[i][k - 1];
           }
-          m[i][j][k] = local_min;
+          m[j][k] = local_min;
         }
-        ComputePrefixSuffixMin(i, j, m, prefix_min, suffix_min);
+      }
+      for (int j = 1; j <= num_neighborhoods; j++) {
+        ComputePrefixSuffixMin(j, m, prefix_min, suffix_min);
       }
     }
 
     int answer = INT_MAX;
     for (int k = 1; k <= num_colors; k++) {
-      answer = min(answer, m[n - 1][num_neighborhoods][k]);
+      answer = min(answer, m[num_neighborhoods][k]);
     }
     return (answer == INT_MAX ? -1 : answer);
   }
 
  private:
-  static void ComputePrefixSuffixMin(const int i, const int j,
-                                     const vector<vector<vector<int>>>& m,
-                                     vector<vector<vector<int>>>& prefix_min,
-                                     vector<vector<vector<int>>>& suffix_min) {
-    const int num_colors = m[i][j].size() - 1;
-    prefix_min[i][j][0] = m[i][j][0];
+  static void ComputePrefixSuffixMin(const int j, const vector<vector<int>>& m,
+                                     vector<vector<int>>& prefix_min,
+                                     vector<vector<int>>& suffix_min) {
+    const int num_colors = m[j].size() - 1;
+    prefix_min[j][0] = m[j][0];
     for (int k = 1; k <= num_colors; k++) {
-      prefix_min[i][j][k] = min(prefix_min[i][j][k - 1], m[i][j][k]);
+      prefix_min[j][k] = min(prefix_min[j][k - 1], m[j][k]);
     }
-    suffix_min[i][j][num_colors] = m[i][j][num_colors];
+    suffix_min[j][num_colors] = m[j][num_colors];
     for (int k = num_colors - 1; k >= 0; k--) {
-      suffix_min[i][j][k] = min(suffix_min[i][j][k + 1], m[i][j][k]);
+      suffix_min[j][k] = min(suffix_min[j][k + 1], m[j][k]);
     }
   }
 };
