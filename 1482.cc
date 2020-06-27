b5f3dafe531bea11260c57c979a7999a9e3cdedf
Jingyue Wu <wujingyue@gmail.com>
2020-06-26 22:04:15 -0700

    More tests.

--- 1482.cc
+++ 1482.cc
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-class Solution {
+class BinarySearchSolution {
  public:
   int minDays(const vector<int>& days, const int m, const int k) {
     int min_day = INT_MAX;
@@ -58,7 +58,29 @@ class Solution {
   }
 };
 
-TEST(SolutionTest, testSample) {
+using Solution = BinarySearchSolution;
+
+TEST(SolutionTest, testSample1) {
   Solution s;
   EXPECT_EQ(3, s.minDays({1, 10, 3, 10, 2}, 3, 1));
+}
+
+TEST(SolutionTest, testSample2) {
+  Solution s;
+  EXPECT_EQ(-1, s.minDays({1, 10, 3, 10, 2}, 3, 2));
+}
+
+TEST(SolutionTest, testSample3) {
+  Solution s;
+  EXPECT_EQ(12, s.minDays({7, 7, 7, 7, 12, 7, 7}, 2, 3));
+}
+
+TEST(SolutionTest, testSample4) {
+  Solution s;
+  EXPECT_EQ(1000000000, s.minDays({1000000000, 1000000000}, 1, 1));
+}
+
+TEST(SolutionTest, testSample5) {
+  Solution s;
+  EXPECT_EQ(9, s.minDays({1, 10, 2, 9, 3, 8, 4, 7, 5, 6}, 4, 2));
 }
