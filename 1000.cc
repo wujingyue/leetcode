dab0c1fc49d68a7a4f8cbc86ff0835c34e8811e4
Jingyue Wu <wujingyue@gmail.com>
2020-07-05 13:24:00 -0700

    Combine m and m2.

--- 1000.cc
+++ 1000.cc
@@ -8,7 +8,7 @@ using namespace std;
 
 class Solution {
  public:
-  int mergeStones(const vector<int>& a, const int k) {
+  int mergeStones(const vector<int>& a, const int num_splits) {
     const int n = a.size();
 
     vector<vector<int>> sum(n, vector<int>(n));
@@ -19,48 +19,41 @@ class Solution {
       }
     }
 
-    vector<vector<int>> m(n, vector<int>(n, INT_MAX));
+    // m[i][j][k] represents the minimum cost to merge the stones between i and
+    // j into k piles.
+    vector<vector<vector<int>>> m(
+        n, vector<vector<int>>(n, vector<int>(num_splits + 1, INT_MAX)));
     for (int i = 0; i < n; i++) {
-      m[i][i] = 0;
+      m[i][i][1] = 0;
     }
 
     for (int i = n - 1; i >= 0; i--) {
       for (int j = i + 1; j < n; j++) {
-        vector<vector<int>> m2 = ComputeM2(m, k, i, j);
-        m[i][j] = (m2[j][k] == INT_MAX ? INT_MAX : m2[j][k] + sum[i][j]);
+        for (int k = 2; k <= num_splits; k++) {
+          int mijk = INT_MAX;
+          for (int t = i; t < j; t++) {
+            const int left_merged_to_one_pile = m[i][t][1];
+            if (left_merged_to_one_pile == INT_MAX) {
+              continue;
+            }
+            const int right = m[t + 1][j][k - 1];
+            if (right == INT_MAX) {
+              continue;
+            }
+            UpdateIfSmaller(mijk, left_merged_to_one_pile + right);
+          }
+          m[i][j][k] = mijk;
+        }
+        if (m[i][j][num_splits] < INT_MAX) {
+          m[i][j][1] = m[i][j][num_splits] + sum[i][j];
+        }
       }
     }
 
-    return m[0][n - 1] == INT_MAX ? -1 : m[0][n - 1];
+    return m[0][n - 1][1] == INT_MAX ? -1 : m[0][n - 1][1];
   }
 
  private:
-  vector<vector<int>> ComputeM2(const vector<vector<int>>& m,
-                                const int k, const int left,
-                                const int right) {
-    vector<vector<int>> m2(right + 1, vector<int>(k + 1));
-    for (int i = left; i < right; i++) {
-      m2[i][1] = m[left][i];
-    }
-    m2[right][1] = INT_MAX;
-    for (int j = 2; j <= k; j++) {
-      for (int i = left; i <= right; i++) {
-        int m2ij = INT_MAX;
-        // count(left..i2) >= j - 1
-        // i2 - left + 1 >= j - 1
-        // i2 >= left + j - 2
-        for (int i2 = left + j - 2; i2 < i; i2++) {
-          if (m2[i2][j - 1] < INT_MAX && m[i2 + 1][i] < INT_MAX) {
-            UpdateIfSmaller(m2ij, m2[i2][j - 1] + m[i2 + 1][i]);
-          }
-        }
-        m2[i][j] = m2ij;
-      }
-    }
-
-    return m2;
-  }
-
   static void UpdateIfSmaller(int& a, int b) {
     if (b < a) {
       a = b;
