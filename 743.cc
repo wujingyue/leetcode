06f0ce25ee81a2bc174185bc739c0e0b8cc51fb2
Jingyue Wu <wujingyue@gmail.com>
2020-07-18 23:36:19 -0700

    Dijkstra without lazy deletion.

--- 743.cc
+++ 743.cc
@@ -12,14 +12,100 @@ struct Neighbor {
   int d;
 };
 
-struct NodeDistance {
-  int x;
+struct NodeInfo {
   int d;
+  int pos_in_pq;
 
-  bool operator<(const NodeDistance& other) const { return other.d < d; }
+  bool operator<(const NodeInfo& other) const { return d < other.d; }
 };
 
 using Graph = vector<vector<Neighbor>>;
+
+class PriorityQueue {
+ public:
+  PriorityQueue(vector<NodeInfo>&& nodes) { nodes_.swap(nodes); }
+  PriorityQueue(const PriorityQueue&) = delete;
+  PriorityQueue& operator=(const PriorityQueue&) = delete;
+  PriorityQueue(PriorityQueue&&) = default;
+
+  NodeInfo* GetNode(const int x) { return &nodes_[x]; }
+
+  void Insert(const int x) {
+    const int i = order_.size();
+    order_.push_back(x);
+    nodes_[x].pos_in_pq = i;
+    PercolateUp(i);
+  }
+
+  int Pop() {
+    const int x = order_[0];
+    nodes_[x].pos_in_pq = -1;
+
+    const int y = order_.back();
+    order_.pop_back();
+
+    if (!order_.empty()) {
+      order_[0] = y;
+      nodes_[y].pos_in_pq = 0;
+      PercolateDown(0);
+    }
+
+    return x;
+  }
+
+  bool Empty() const { return order_.empty(); }
+
+  void Adjust(const int i) {
+    PercolateUp(i);
+    PercolateDown(i);
+  }
+
+ private:
+  bool SatisfiesHeapOrder(const int x, const int y) {
+    return !(nodes_[y] < nodes_[x]);
+  }
+
+  void PercolateUp(int j) {
+    int y = order_[j];
+    while (j > 0) {
+      int i = (j - 1) / 2;
+      int x = order_[i];
+      if (SatisfiesHeapOrder(x, y)) {
+        break;
+      }
+      order_[j] = x;
+      nodes_[x].pos_in_pq = j;
+      j = i;
+    }
+    order_[j] = y;
+    nodes_[y].pos_in_pq = j;
+  }
+
+  void PercolateDown(int i) {
+    const int n = order_.size();
+
+    int x = order_[i];
+    while (i * 2 + 1 < n) {
+      int j = i * 2 + 1;
+      if (j + 1 < (int)order_.size() &&
+          nodes_[order_[j + 1]] < nodes_[order_[j]]) {
+        j++;
+      }
+      int y = order_[j];
+      if (SatisfiesHeapOrder(x, y)) {
+        break;
+      }
+      order_[i] = y;
+      nodes_[y].pos_in_pq = i;
+      i = j;
+    }
+    order_[i] = x;
+    nodes_[x].pos_in_pq = i;
+  }
+
+  vector<int> order_;
+  vector<NodeInfo> nodes_;
+};
 
 class Solution {
  public:
@@ -34,44 +120,55 @@ class Solution {
       g[x].push_back(Neighbor{y, edge[2]});
     }
 
-    vector<int> d = Dijkstra(g, start);
-    const int max_d = *max_element(d.begin(), d.end());
+    PriorityQueue pq = Dijkstra(g, start);
+    int max_d = INT_MIN;
+    for (int x = 0; x < n; x++) {
+      max_d = max(max_d, pq.GetNode(x)->d);
+    }
     return max_d == INT_MAX ? -1 : max_d;
   }
 
  private:
-  vector<int> Dijkstra(const Graph& g, const int start) {
+  PriorityQueue Dijkstra(const Graph& g, const int start) {
     const int n = g.size();
-    vector<int> d(n, INT_MAX);
-    priority_queue<NodeDistance> pq;
-    vector<int> expanded(n, false);
 
-    pq.push(NodeDistance{start, 0});
-    d[start] = 0;
+    vector<NodeInfo> nodes;
+    nodes.reserve(n);
+    for (int x = 0; x < n; x++) {
+      nodes.push_back(NodeInfo{x == start ? 0 : INT_MAX, -1});
+    }
 
-    while (!pq.empty()) {
-      const NodeDistance top = pq.top();
-      pq.pop();
+    PriorityQueue pq(std::move(nodes));
+    pq.Insert(start);
 
-      if (expanded[top.x]) {
-        continue;
-      }
-      expanded[top.x] = true;
+    while (!pq.Empty()) {
+      const int x = pq.Pop();
 
-      for (const Neighbor& neighbor : g[top.x]) {
-        const int new_d = top.d + neighbor.d;
-        if (new_d < d[neighbor.y]) {
-          d[neighbor.y] = new_d;
-          pq.push(NodeDistance{neighbor.y, new_d});
+      for (const Neighbor& neighbor : g[x]) {
+        NodeInfo* node_y = pq.GetNode(neighbor.y);
+        const int new_d = pq.GetNode(x)->d + neighbor.d;
+        if (new_d < node_y->d) {
+          node_y->d = new_d;
+          const int j = node_y->pos_in_pq;
+          if (j == -1) {
+            pq.Insert(neighbor.y);
+          } else {
+            pq.Adjust(j);
+          }
         }
       }
     }
 
-    return d;
+    return pq;
   }
 };
 
 TEST(SolutionTest, testSample) {
   Solution s;
   EXPECT_EQ(2, s.networkDelayTime({{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}, 4, 2));
+}
+
+TEST(SolutionTest, testNotConnected) {
+  Solution s;
+  EXPECT_EQ(-1, s.networkDelayTime({}, 2, 1));
 }
