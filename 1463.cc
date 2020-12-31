fead680e966bfcf190abf7073ab12bda64fab1db
Jingyue Wu <wujingyue@gmail.com>
2020-12-30 17:45:55 -0800

    Optimized the space complexity to O(c^2).

--- 1463.cc
+++ 1463.cc
@@ -10,13 +10,14 @@ class Solution {
   int cherryPickup(const vector<vector<int>>& a) {
     const int r = a.size();
     const int c = a[0].size();
-    vector<vector<vector<int>>> m(
-        r, vector<vector<int>>(c, vector<int>(c, INT_MIN)));
-    m[0][0][c - 1] = a[0][0] + a[0][c - 1];
+    vector<vector<int>> m(c, vector<int>(c, INT_MIN));
+    vector<vector<int>> m_prime(c, vector<int>(c, INT_MIN));
+    m[0][c - 1] = a[0][0] + a[0][c - 1];
     for (int x = 0; x < r - 1; x++) {
       for (int y1 = 0; y1 < c; y1++) {
         for (int y2 = y1; y2 < c; y2++) {
-          if (m[x][y1][y2] == INT_MIN) {
+          const int old_sum = m[y1][y2];
+          if (old_sum == INT_MIN) {
             continue;
           }
           for (int dy1 = -1; dy1 <= 1; dy1++) {
@@ -29,21 +30,25 @@ class Solution {
               if (y2_prime < y1_prime || y2_prime >= c) {
                 continue;
               }
-              const int m_prime =
-                  m[x][y1][y2] + a[x + 1][y1_prime] +
+              const int new_sum =
+                  old_sum + a[x + 1][y1_prime] +
                   (y1_prime == y2_prime ? 0 : a[x + 1][y2_prime]);
-              m[x + 1][y1_prime][y2_prime] =
-                  max(m[x + 1][y1_prime][y2_prime], m_prime);
+              int& m_prime_ref = m_prime[y1_prime][y2_prime];
+              m_prime_ref = max(m_prime_ref, new_sum);
             }
           }
         }
+      }
+      m.swap(m_prime);
+      for (int y1 = 0; y1 < c; y1++) {
+        fill(m_prime[y1].begin(), m_prime[y1].end(), INT_MIN);
       }
     }
 
     int max_sum = 0;
     for (int y1 = 0; y1 < c; y1++) {
       for (int y2 = y1; y2 < c; y2++) {
-        max_sum = max(max_sum, m[r - 1][y1][y2]);
+        max_sum = max(max_sum, m[y1][y2]);
       }
     }
     return max_sum;
