0304aced0217bba6d8124c060372d13f4d011c92
Jingyue Wu <wujingyue@gmail.com>
2022-02-22 11:57:02 -0800

    Fast solution using a complete binary search tree.

--- 327.cc
+++ 327.cc
@@ -1,35 +1,105 @@
 #include <algorithm>
 #include <cassert>
+#include <climits>
 #include <vector>
 
 #include "gtest/gtest.h"
 
 using namespace std;
 
+struct Node {
+  int64_t value;
+  int size;  // The size of the subtree rooted at the node.
+};
+
 class OrderedMultiset {
  public:
-  OrderedMultiset(vector<int64_t>&& values)
-      : values_(values), counts_(values.size()) {}
+  // Construct a complete binary search tree with a list of pre-defined values.
+  OrderedMultiset(const vector<int64_t>& sorted_values);
+
+  // The caller must make sure `value` was in the constructor's `sorted_values`.
   void Insert(int64_t value);
+
+  // Unlike `Insert`, `value` doesn't have to be in `sorted_values`.
   int CountLessThan(int64_t value) const;
 
  private:
-  vector<int64_t> values_;
-  vector<int> counts_;
+  void BuildCompleteBinarySearchTree(const vector<int64_t>& sorted_values,
+                                     int index, int left, int right);
+
+  vector<Node> nodes_;
 };
 
+OrderedMultiset::OrderedMultiset(const vector<int64_t>& sorted_values) {
+  const int n = sorted_values.size();
+  nodes_.resize(n);
+  BuildCompleteBinarySearchTree(sorted_values, 0, 0, n - 1);
+}
+
+void OrderedMultiset::BuildCompleteBinarySearchTree(
+    const vector<int64_t>& sorted_values, const int index, const int low,
+    const int high) {
+  const int size = high - low + 1;
+  if (size == 0) {
+    return;
+  }
+
+  if (size == 1) {
+    nodes_[index].value = sorted_values[low];
+    return;
+  }
+
+  // height = floor(log_2(size)) + 1
+  const int height = sizeof(int) * CHAR_BIT - __builtin_clz(size);
+  // assert(height >= 2 && "height >= 2 because size >= 2");
+  int left_size = (1 << (height - 2)) - 1;
+  int right_size = left_size;
+  const int remaining = size - 1 - left_size - right_size;
+  if (remaining <= (1 << (height - 2))) {
+    left_size += remaining;
+  } else {
+    left_size += (1 << (height - 2));
+    right_size += remaining - (1 << (height - 2));
+  }
+
+  nodes_[index].value = sorted_values[low + left_size];
+  BuildCompleteBinarySearchTree(sorted_values, index * 2 + 1, low,
+                                low + left_size - 1);
+  BuildCompleteBinarySearchTree(sorted_values, index * 2 + 2,
+                                low + left_size + 1, high);
+}
+
 void OrderedMultiset::Insert(const int64_t value) {
-  const int i =
-      lower_bound(values_.begin(), values_.end(), value) - values_.begin();
-  assert(i < (int)values_.size());
-  assert(values_[i] == value);
-  counts_[i]++;
+  int i = 0;
+  while (true) {
+    // assert(i < (int)nodes_.size() &&
+    //        "Otherwise `value` was not passed into the constructor.");
+    nodes_[i].size++;
+    const int64_t curr = nodes_[i].value;
+    if (value == curr) {
+      break;
+    }
+    if (value < curr) {
+      i = i * 2 + 1;
+    } else {
+      i = i * 2 + 2;
+    }
+  }
 }
 
 int OrderedMultiset::CountLessThan(const int64_t value) const {
+  const int n = nodes_.size();
   int count = 0;
-  for (int i = 0, size = values_.size(); i < size && values_[i] < value; i++) {
-    count += counts_[i];
+  int i = 0;
+  while (i < n) {
+    const int64_t curr = nodes_[i].value;
+    if (value <= curr) {
+      i = i * 2 + 1;
+    } else {
+      const int right = i * 2 + 2;
+      count += nodes_[i].size - (right < n ? nodes_[right].size : 0);
+      i = right;
+    }
   }
   return count;
 }
@@ -51,15 +121,18 @@ class Solution {
     prefix_sums.erase(unique(prefix_sums.begin(), prefix_sums.end()),
                       prefix_sums.end());
 
-    OrderedMultiset ordered_multiset(std::move(prefix_sums));
+    OrderedMultiset ordered_multiset(prefix_sums);
 
     int count = 0;
     prefix_sum = 0;
-    for (int num : nums) {
+    for (const int num : nums) {
+      // Insert last prefix sum.
       ordered_multiset.Insert(prefix_sum);
-      // lower <= prefix_sum - previous_prefix_sum <= upper
-      // prefix_sum - upper <= previous_prefix_sum < prefix_sum - lower + 1
       prefix_sum += num;
+      // Find the number of previous prefix sums that satisfy
+      //   lower <= prefix_sum - previous_prefix_sum <= upper
+      // i.e.
+      //   prefix_sum - upper <= previous_prefix_sum < prefix_sum - lower + 1
       count += ordered_multiset.CountLessThan(prefix_sum - lower + 1) -
                ordered_multiset.CountLessThan(prefix_sum - upper);
     }
